858e2c383d8901d3f8067e0e76c04f1afca03887
Saman Ghannadzadeh <saman@saman-gh.co.uk>
2022-12-22 09:12:46 +0000

    Update imgui

--- include/udaq/helpers/imgui/wrapper.h
+++ include/udaq/helpers/imgui/wrapper.h
@@ -1,6 +1,13 @@
+#pragma once
+
 #include <imgui.h>
 #include <backends/imgui_impl_sdl.h>
 #include <backends/imgui_impl_opengl3.h>
 #include <stdio.h>
 #include <SDL.h>
-#include <GL/gl3w.h>
+
+#if defined(IMGUI_IMPL_OPENGL_ES2)
+#include <SDL_opengles2.h>
+#else
+#include <SDL_opengl.h>
+#endif
